e7db56e53b8c88482d5cce42490a23f31784c58b
Ian Baker <inbox@ianbaker.me>
2025-07-07 22:20:19 +0100

    Refactored to use StringBuilder

--- FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz.cs
@@ -1,21 +1,37 @@
 ﻿namespace FizzBuzz
 {
+    using System.Text;
+
     public static class FizzBuzz
     {
+        private const string Fizz = "Fizz";
+        private const string Buzz = "Buzz";
+
         public static string Process(int index)
         {
             if (index < 1 || index > 100)
+            {
                 throw new ArgumentOutOfRangeException(nameof(index));
+            }
 
-            var response = "";
+            var response = new StringBuilder();
             
             if (index % 3 == 0)
-                response += "Fizz";
+            {
+                response.Append(Fizz);
+            }
 
             if (index % 5 == 0)
-                response += "Buzz";
+            {
+                response.Append(Buzz);
+            }
 
-            return response.Length == 0 ? index.ToString() : response; 
+            if (string.IsNullOrEmpty(response.ToString()))
+            {
+                response.Append(index);
+            }
+
+            return response.ToString(); 
         }
     }
 }
